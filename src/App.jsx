99bd9fb3e61221093c48d20b0aa8f572e79ce1ec
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-06-29 14:08:58 +0300

    list of goods

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import cn from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -14,70 +16,85 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+const arrOfKeys = [];
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+function setRandomKey() {
+  const randomKey = Math.floor(Math.random() * 1000);
 
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+  if (!arrOfKeys.includes(randomKey)) {
+    arrOfKeys.push(randomKey);
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+    return randomKey;
+  }
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+  return setRandomKey();
+}
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
+export const App = () => {
+  const [selectedGood, setSelectedGood] = useState('Jam');
+  const selectedTitle = selectedGood
+    ? (
+      <h1 className="title is-flex is-align-items-center">
+        {`${selectedGood} is selected`}
+        <button
+          data-cy="ClearButton"
+          type="button"
+          className="delete ml-3"
+          onClick={() => setSelectedGood('')}
+        />
+      </h1>
+    )
+    : (
+      <h1 className="title is-flex is-align-items-center">
+        No goods selected
+      </h1>
+    );
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
+  return (
+    <main className="section container">
+      {selectedTitle}
+      <table className="table">
+        <tbody>
+          {goods.map((good) => {
+            const selected = good === selectedGood;
 
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+            return (
+              <tr
+                key={setRandomKey()}
+                data-cy="Good"
+                className={cn('', {
+                  'has-background-success-light': selected,
+                })}
+              >
+                <td>
+                  <button
+                    data-cy={selected
+                      ? 'RemoveButton'
+                      : 'AddButton'}
+                    type="button"
+                    onClick={() => (selected
+                      ? setSelectedGood('')
+                      : setSelectedGood((good)))}
+                    className={cn('button', {
+                      'is-info': selected,
+                    })}
+                  >
+                    {
+                      selected
+                        ? '-'
+                        : '+'
+                    }
+                  </button>
+                </td>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
+  );
+};
