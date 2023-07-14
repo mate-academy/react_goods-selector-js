f15be183571d34a787757421472cbe629eda056c
Absolution26 <zet2571428@gmail.com>
2023-07-15 02:41:10 +0300

    create separate function

--- src/App.jsx
+++ src/App.jsx
@@ -19,28 +19,27 @@ export const goods = [
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
 
+  function removeGood() {
+    return setSelectedGood('');
+  }
+
   return (
     <main className="section container">
-      {selectedGood === ''
-        ? (
-          <h1 className="title is-flex is-align-items-center">
-            No goods selected
-          </h1>
-        )
-        : (
-          <h1 className="title is-flex is-align-items-center">
-            {`${selectedGood} is selected`}
+      <h1 className="title is-flex is-align-items-center">
+        { selectedGood === ''
+          ? 'No goods selected'
+          : `${selectedGood} is selected`
+        }
 
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={() => {
-                setSelectedGood('');
-              }}
-            />
-          </h1>
+        {selectedGood && (
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={removeGood}
+          />
         )}
+      </h1>
 
       <table className="table">
         <tbody>
