982a27552284c850838dd6541bee7e0b4b8cff5f
Dima Astapenko <bonum_taurus@ukr.net>
2023-07-24 15:57:43 +0300

    don`t found solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goods = [
   'Dumplings',
@@ -13,6 +14,59 @@ export const goods = [
   'Jam',
   'Garlic',
 ];
+
+export const Good = ({ good }) => {
+  const [buttonClass, setButtonClass] = useState('button');
+  const [bg, setBg] = useState('');
+  const [operator, setOperator] = useState('+');
+  const [button, setButton] = useState('AddButton');
+
+  return (
+    <tr
+      data-cy="Good"
+      className={bg}
+    >
+      <td>
+        <button
+          onClick={(event) => {
+            if (buttonClass === 'button is-info') {
+              setButtonClass(`button`);
+            } else {
+              setButtonClass('button is-info');
+            }
+
+            if (operator === '-') {
+              setOperator('+');
+            } else {
+              setOperator('-');
+            }
+
+            if (bg === '') {
+              setBg('has-background-success-light');
+            } else {
+              setBg('');
+            }
+
+            if (button === 'AddButton') {
+              setButton('RemoveButton');
+            } else {
+              setButton('AddButton');
+            }
+          }}
+          data-cy={button}
+          type="button"
+          className={buttonClass}
+        >
+          {operator}
+        </button>
+      </td>
+
+      <td data-cy="GoodTitle" className="is-vcentered">
+        {good}
+      </td>
+    </tr>
+  );
+};
 
 export const App = () => (
   <main className="section container">
@@ -30,21 +84,9 @@ export const App = () => (
 
     <table className="table">
       <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+        {goods && goods.map(good => (
+          <Good good={good} key={good} />
+        ))}
 
         <tr data-cy="Good" className="has-background-success-light">
           <td>
