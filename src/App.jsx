1ae25f5823024adcecb7d19fad80674bf32943df
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-07-25 12:21:26 +0200

    implement task good-selector

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,38 @@ export const App = () => {
               selected={goodSelected}
             />
           ))}
+          {/*
+          <tr data-cy="Good" className="has-background-success-light">
+            <td>
+              <button
+                data-cy="RemoveButton"
+                type="button"
+                className="button is-info"
+              >
+                -
+              </button>
+            </td>
+
+            <td data-cy="GoodTitle" className="is-vcentered">
+              Jam
+            </td>
+          </tr>
+
+          <tr data-cy="Good">
+            <td>
+              <button
+                data-cy="AddButton"
+                type="button"
+                className="button"
+              >
+                +
+              </button>
+            </td>
+
+            <td data-cy="GoodTitle" className="is-vcentered">
+              Garlic
+            </td>
+          </tr> */}
         </tbody>
       </table>
     </main>
--- src/components/GoodCard.jsx
+++ src/components/GoodCard.jsx
@@ -1,19 +1,30 @@
 export const GoodCard = ({ good, callback, selected }) => (
-  <tr data-cy="Good">
-    {selected === '' && (
+  <tr data-cy="Good" className={`${good === selected && 'has-background-success-light'}`}>
+    {good !== selected ? (
       <td>
         <button
           data-cy="AddButton"
           type="button"
-          className={`button ${good === selected && 'is-info'}`}
+          className="button"
           onClick={value => callback(good)}
         >
           +
         </button>
       </td>
+    ) : (
+      <td>
+        <button
+          data-cy="RemoveButton"
+          type="button"
+          className="button is-info"
+          onClick={() => callback('')}
+        >
+          -
+        </button>
+      </td>
     )}
 
-    <td data-cy="GoodTitle" className={`is-vcentered ${good === selected && 'has-background-success-light'}`}>
+    <td data-cy="GoodTitle" className="is-vcentered">
       {good}
     </td>
   </tr>
