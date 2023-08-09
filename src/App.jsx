b1b12573cfb7507b1e41ed5353a1ae464fbc9184
Dmytro <dmytro2105@gmail.com>
2023-08-09 17:26:51 +0300

    Goods selector

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,9 @@ export const App = () => {
   return (
     <main className="section container">
       {!selectedGood ? (
-        <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+        <h1 className="title is-flex is-align-items-center">
+          No goods selected
+        </h1>
       ) : (
 
         <h1 className="title is-flex is-align-items-center">
@@ -44,8 +46,8 @@ export const App = () => {
             <tr
               data-cy="Good"
               key={good}
-              className={selectedGood === good &&
-                'has-background-success-light'}
+              className={selectedGood === good
+                && 'has-background-success-light'}
             >
               <td>
                 {selectedGood === good
@@ -74,16 +76,6 @@ export const App = () => {
                     </button>
                   )
                 }
-                {/* <button
-                  data-cy="AddButton"
-                  type="button"
-                  className="button"
-                  onClick={() => {
-                    setGood(good);
-                  }}
-                >
-                  +
-                </button> */}
               </td>
 
               <td data-cy="GoodTitle" className="is-vcentered">
@@ -91,54 +83,6 @@ export const App = () => {
               </td>
             </tr>
           ))}
-
-          <tr data-cy="Good">
-            <td>
-              <button
-                data-cy="AddButton"
-                type="button"
-                className="button"
-              >
-                +
-              </button>
-            </td>
-
-            <td data-cy="GoodTitle" className="is-vcentered">
-              Dumplings
-            </td>
-          </tr>
-
-          <tr data-cy="Good" className="has-background-success-light">
-            <td>
-              <button
-                data-cy="RemoveButton"
-                type="button"
-                className="button is-info"
-              >
-                -
-              </button>
-            </td>
-
-            <td data-cy="GoodTitle" className="is-vcentered">
-              Jam
-            </td>
-          </tr>
-
-          <tr data-cy="Good">
-            <td>
-              <button
-                data-cy="AddButton"
-                type="button"
-                className="button"
-              >
-                +
-              </button>
-            </td>
-
-            <td data-cy="GoodTitle" className="is-vcentered">
-              Garlic
-            </td>
-          </tr>
         </tbody>
       </table>
     </main>
