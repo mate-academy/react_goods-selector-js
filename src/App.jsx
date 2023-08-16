9e71b4c24edd7b44f819a09ac16b3602dc5430f9
Denis Hurskiy <denis.gurskiy@gmail.com>
2023-08-16 12:15:22 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -19,39 +19,46 @@ export const goods = [
 export const App = () => {
   const [value, setValue] = useState('Jam');
 
+  const setNull = () => {
+    setValue('');
+  };
+
   return (
     <main className="section container">
-      {value === ''
-        ? (
-          <h1 className="title is-flex is-align-items-center">
-            No goods selected
-          </h1>
-        )
-        : (
-          <h1 className="title is-flex is-align-items-center">
-            {value}
-            {' '}
-            is selected
+      <h1 className="title is-flex is-align-items-center">
+        {!value
+          ? (
+            <>No goods selected</>
+          )
+          : (
+            <>
+              {value}
+              {' '}
+              is selected
 
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={() => setValue('')}
-            />
-          </h1>
-        )
-      }
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={setNull}
+              />
+            </>
+          )
+        }
+      </h1>
 
       <table className="table">
         <tbody>
-          {goods.map(elem => (
+          {goods.map((elem, index) => (
             <tr
               data-cy="Good"
-              className={cn({ 'has-background-success-light': elem === value })}
+              className={cn(
+                { 'has-background-success-light': elem === value },
+              )}
+              key={elem}
             >
               <td>
-                { elem !== value
+                {elem !== value
                   ? (
                     <button
                       data-cy="AddButton"
@@ -67,7 +74,7 @@ export const App = () => {
                       data-cy="RemoveButton"
                       type="button"
                       className="button is-info"
-                      onClick={() => setValue('')}
+                      onClick={setNull}
                     >
                       -
                     </button>
