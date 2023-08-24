10b37d89c6abd47dd6a4e5ff085ba807b4103340
Vladyslav Prokhasko <vlad56445@gmail.com>
2023-08-24 20:24:01 +0300

    Improved code readability and structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -15,75 +15,95 @@ export const goods = [
   'Garlic',
 ];
 
+const defaultSelectedGood = 'Jam';
+const emptySelectedGood = '';
+const noGoodsSelectedMessage = 'No goods selected';
+const selectedBackgroundClass = 'has-background-success-light';
+
 export const App = () => {
-  const [selectedGood, setSelectedGood] = useState('Jam');
+  const [selectedGood, setSelectedGood] = useState(defaultSelectedGood);
+  const isSelectedGoodText = `${selectedGood} is selected`;
 
   const handleSelectedGood = (good) => {
     setSelectedGood(good);
   };
 
   const handleClearSelection = () => {
-    setSelectedGood('');
+    setSelectedGood(emptySelectedGood);
   };
+
+  const isGoodSelected = selectedGood !== emptySelectedGood;
+
+  const clearButton = (
+    <button
+      data-cy="ClearButton"
+      type="button"
+      className="delete ml-3"
+      onClick={handleClearSelection}
+    />
+  );
+
+  const addButton = good => (
+    <button
+      data-cy="AddButton"
+      type="button"
+      className="button"
+      onClick={() => handleSelectedGood(good)}
+    >
+      +
+    </button>
+  );
+
+  const removeButton = (
+    <button
+      data-cy="RemoveButton"
+      type="button"
+      className="button is-info"
+      onClick={handleClearSelection}
+    >
+      -
+    </button>
+  );
 
   return (
     <main className="section container">
       <h1 className="title is-flex is-align-items-center">
         {selectedGood
-          ? `${selectedGood} is selected`
-          : 'No goods selected'
+          ? isSelectedGoodText
+          : noGoodsSelectedMessage
         }
 
-        {selectedGood && (
-          <button
-            data-cy="ClearButton"
-            type="button"
-            className="delete ml-3"
-            onClick={handleClearSelection}
-          />
-        )}
+        {isGoodSelected && clearButton}
       </h1>
 
       <table className="table">
         <tbody>
-          {goods.map(good => (
-            <tr
-              key={good}
-              data-cy="Good"
-              className={selectedGood === good
-                ? 'has-background-success-light'
-                : ''
-              }
-            >
-              <td>
-                {selectedGood !== good && (
-                  <button
-                    data-cy="AddButton"
-                    type="button"
-                    className="button"
-                    onClick={() => handleSelectedGood(good)}
-                  >
-                    +
-                  </button>
-                )}
+          {goods.map((good) => {
+            const isSelected = selectedGood === good;
 
-                {selectedGood === good && (
-                  <button
-                    data-cy="RemoveButton"
-                    type="button"
-                    className="button is-info"
-                    onClick={handleClearSelection}
-                  >
-                    -
-                  </button>
-                )}
-              </td>
+            return (
+              <tr
+                key={good}
+                data-cy="Good"
+                className={
+                  isSelected
+                    ? selectedBackgroundClass
+                    : emptySelectedGood
+                }
+              >
+                <td>
+                  {isGoodSelected && isSelected
+                    ? removeButton
+                    : addButton(good)
+                  }
+                </td>
 
-              <td data-cy="GoodTitle" className="is-vcentered">
-                {good}
-              </td>
-            </tr>
-          ))}
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </main>
