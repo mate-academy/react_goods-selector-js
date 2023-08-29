d1be583729f4b6d352a2811e763e92b58805f023
Sergey <serg9205@gmail.com>
2023-08-29 16:37:36 +0300

    add some constants and simplified the code with stateComparationResult variable

--- src/App.jsx
+++ src/App.jsx
@@ -19,20 +19,18 @@ export const goods = [
 const EMPTY_SPACE = '';
 const ADD_BUTTON_TEXT = '+';
 const REMOVE_BUTTON_TEXT = '-';
-const DEFAULT_GOOD_VALUE = 'Jam';
+const SELECTED_GOOD_MESSAGE = ' is selected';
+const NO_GOOD_SELECTED_MESSAGE = 'No goods selected';
 
 export const App = () => {
-  const [goodState, setGoodState] = useState(DEFAULT_GOOD_VALUE);
-
-  const selectedGoodMessage = `${goodState} is selected`;
-  const noGoodSelectedMessage = 'No goods selected';
+  const [goodState, setGoodState] = useState(goods[8]);
 
   return (
     <main className="section container">
       <h1 className="title is-flex is-align-items-center">
         {goodState.length ? (
           <>
-            {selectedGoodMessage}
+            {goodState + SELECTED_GOOD_MESSAGE}
             <button
               data-cy="ClearButton"
               type="button"
@@ -43,36 +41,42 @@ export const App = () => {
             />
           </>
         )
-          : noGoodSelectedMessage
+          : NO_GOOD_SELECTED_MESSAGE
         }
       </h1>
       <table className="table">
         <tbody>
-          {goods.map(good => (
-            <tr
-              data-cy="Good"
-              key={good}
-              className={cn({
-                'has-background-success-light': good === goodState,
-              })}
-            >
-              <td>
-                <button
-                  data-cy={`${good === goodState ? 'RemoveButton' : 'AddButton'}`}
-                  type="button"
-                  className={`button ${good === goodState ? 'is-info' : EMPTY_SPACE}`}
-                  onClick={() => {
-                    setGoodState(goodState === good ? EMPTY_SPACE : good);
-                  }}
-                >
-                  {good === goodState ? REMOVE_BUTTON_TEXT : ADD_BUTTON_TEXT}
-                </button>
-              </td>
-              <td data-cy="GoodTitle" className="is-vcentered">
-                {good}
-              </td>
-            </tr>
-          ))}
+          {goods.map((good) => {
+            const stateComprationResult = good === goodState;
+
+            return (
+              <tr
+                data-cy="Good"
+                key={good}
+                className={cn({
+                  'has-background-success-light': stateComprationResult,
+                })}
+              >
+                <td>
+                  <button
+                    data-cy={`${stateComprationResult ? 'RemoveButton' : 'AddButton'}`}
+                    type="button"
+                    className={`button ${stateComprationResult ? 'is-info' : EMPTY_SPACE}`}
+                    onClick={() => {
+                      setGoodState(stateComprationResult ? EMPTY_SPACE : good);
+                    }}
+                  >
+                    {stateComprationResult
+                      ? REMOVE_BUTTON_TEXT
+                      : ADD_BUTTON_TEXT}
+                  </button>
+                </td>
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </main>
