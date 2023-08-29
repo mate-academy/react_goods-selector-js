1f2692b7c4b441484bc5fcb1c485fad8cfd86143
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-08-29 09:53:41 +0300

    fix names and add new logic

--- src/App.jsx
+++ src/App.jsx
@@ -17,82 +17,77 @@ export const goods = [
 ];
 
 export const App = () => {
-  const DEFAULT_GOOD = goods[8];
-  const [titleGood, setTitleGood] = useState(DEFAULT_GOOD);
-  const [noGoodMessage, setNoGoodMessage] = useState(false);
+  const [selectedGood, setSelectedGood] = useState('Jam');
 
   return (
     <main className="section container">
-      { noGoodMessage
-        ? (
-          <h1 className="title is-flex is-align-items-center">
-            No goods selected
-          </h1>
-        ) : (
-          <h1 className="title is-flex is-align-items-center">
-            {`${titleGood} is selected`}
 
-            <button
-              onClick={() => {
-                setNoGoodMessage(true);
-                setTitleGood('');
-              }}
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-            />
-          </h1>
-        )
-      }
+      <h1 className="title is-flex is-align-items-center">
+        {selectedGood.length < 1
+          ? (
+            'No goods selected'
+          ) : (
+            <>
+              {`${selectedGood} is selected`}
+
+              <button
+                onClick={() => {
+                  setSelectedGood('');
+                }}
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+              />
+            </>
+          )}
+      </h1>
 
       <table className="table">
         <tbody>
 
           {
-          goods.map(good => (
-            <tr
-              className={cn('', {
-                'has-background-success-light': titleGood === good,
-              })}
-              data-cy="Good"
-              key={good}
-            >
-              <td>
-                { titleGood === good
-                  ? (
-                    <button
-                      onClick={() => {
-                        setTitleGood('');
-                        setNoGoodMessage(true);
-                      }}
-                      data-cy="RemoveButton"
-                      type="button"
-                      className="button is-info"
-                    >
-                      -
-                    </button>
-                  ) : (
-                    <button
-                      onClick={() => {
-                        setTitleGood(good);
-                        setNoGoodMessage(false);
-                      }}
-                      data-cy="AddButton"
-                      type="button"
-                      className="button"
-                    >
-                      +
-                    </button>
-                  )
+            goods.map(good => (
+              <tr
+                className={cn('', {
+                  'has-background-success-light': selectedGood === good,
+                })}
+                data-cy="Good"
+                key={good}
+              >
+                <td>
+                  {selectedGood === good
+                    ? (
+                      <button
+                        onClick={() => {
+                          setSelectedGood('');
+                        }}
+                        data-cy="RemoveButton"
+                        type="button"
+                        className="button is-info"
+                      >
+                        -
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => {
+                          setSelectedGood(good);
+                        }}
+                        data-cy="AddButton"
+                        type="button"
+                        className="button"
+                      >
+                        +
+                      </button>
+                    )
 
-                }
-              </td>
+                  }
+                </td>
 
-              <td data-cy="GoodTitle" className="is-vcentered">
-                {good}
-              </td>
-            </tr>
-          ))
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            ))
           }
         </tbody>
       </table>
