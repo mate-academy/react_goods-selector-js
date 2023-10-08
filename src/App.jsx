8fec14d1bf3261af230ce5bd7d4a18e3487c4842
Lubomur Chuchvara <lubomircucvara@gmail.com>
2023-10-08 14:29:24 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+// eslint-disable-next-line no-unused-vars
+import classNames from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -14,70 +17,59 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [item, setItem] = useState('Jam');
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+  return (
+    <main className="section containe">
+      {item ? (
+        <h1 className="title is-flex is-align-items-center">
+          {`${item} is selected`}
 
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+          <button
+            data-cy="Addbuton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => setItem('')}
+          />
+        </h1>
+      ) : (
+        <h1 className="title is-flex is-align-items-center">
+          No goods selected
+        </h1>
+      )}
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+      <table className="table">
+        <tbody>
+          {goods.map((nameofen) => {
+            const isActive = nameofen === item;
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
-
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+            return (
+              <tr
+                data-cy="Good"
+                key={nameofen}
+                className={`${
+                  isActive ? 'has-background-success-light' : ''
+                }`}
+              >
+                <td>
+                  <button
+                    data-cy={isActive ? 'RemoveButton' : 'AddButton'}
+                    type="button"
+                    className={`button ${isActive ? 'is-info' : ''}`}
+                    onClick={() => setItem(isActive ? '' : nameofen)}
+                  >
+                    {isActive ? '-' : '+'}
+                  </button>
+                </td>
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {nameofen}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import { useState } from "react";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+export const [item, setItem] = useState("Jam");
