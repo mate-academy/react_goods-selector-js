d549445d0aab0347dd1625b604c0f14648b5407b
BetterSol <94358102+BetterSol@users.noreply.github.com>
2023-10-09 23:59:05 +0300

    comments removed

--- src/App.jsx
+++ src/App.jsx
@@ -20,24 +20,20 @@ export const App = () => {
 
   return (
     <main className="section container">
-      {!selectedGood
-        ? (
-          <h1 className="title is-flex is-align-items-center">
-            No goods selected
-          </h1>
-        ) : (
-          <h1 className="title is-flex is-align-items-center">
-            {`${selectedGood} is selected`}
-
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={() => setSelectedGood('')}
-            />
-          </h1>
-        )
-      }
+      <h1 className="title is-flex is-align-items-center">
+        {selectedGood
+          ? `${selectedGood} is selected`
+          : 'No goods selected'
+        }
+        {selectedGood && (
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => setSelectedGood('')}
+          />
+        )}
+      </h1>
 
       <table className="table">
         <tbody>
@@ -84,71 +80,3 @@ export const App = () => {
     </main>
   );
 };
-
-// export const App = () => (
-//   <main className="section container">
-//     <h1 className="title is-flex is-align-items-center">No goods selected</h1>
-
-//     <h1 className="title is-flex is-align-items-center">
-//       Jam is selected
-
-//       <button
-//         data-cy="ClearButton"
-//         type="button"
-//         className="delete ml-3"
-//       />
-//     </h1>
-
-//     <table className="table">
-//       <tbody>
-//         <tr data-cy="Good">
-//           <td>
-//             <button
-//               data-cy="AddButton"
-//               type="button"
-//               className="button"
-//             >
-//               +
-//             </button>
-//           </td>
-
-//           <td data-cy="GoodTitle" className="is-vcentered">
-//             Dumplings
-//           </td>
-//         </tr>
-
-//         <tr data-cy="Good" className="has-background-success-light">
-//           <td>
-//             <button
-//               data-cy="RemoveButton"
-//               type="button"
-//               className="button is-info"
-//             >
-//               -
-//             </button>
-//           </td>
-
-//           <td data-cy="GoodTitle" className="is-vcentered">
-//             Jam
-//           </td>
-//         </tr>
-
-//         <tr data-cy="Good">
-//           <td>
-//             <button
-//               data-cy="AddButton"
-//               type="button"
-//               className="button"
-//             >
-//               +
-//             </button>
-//           </td>
-
-//           <td data-cy="GoodTitle" className="is-vcentered">
-//             Garlic
-//           </td>
-//         </tr>
-//       </tbody>
-//     </table>
-//   </main>
-// );
