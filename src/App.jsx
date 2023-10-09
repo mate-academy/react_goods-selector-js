2d71569639ff0ea51465693b01df01f3d9c5f78e
Oleh <olezhkapopovych@gmail.com>
2023-10-09 17:27:14 +0300

    add boolean variable

--- src/App.jsx
+++ src/App.jsx
@@ -52,45 +52,49 @@ export const App = () => {
 
       <table className="table">
         <tbody>
-          {goods.map(good => (
-            <tr
-              data-cy="Good"
-              key={uuidv4()}
-              className={
-                classes({ 'has-background-success-light': good === title })
-              }
-            >
-              <td>
-                <button
-                  data-cy={good === title
-                    ? 'RemoveButton'
-                    : 'AddButton'
-                  }
-                  aria-label={good === title
-                    ? 'clearTitle'
-                    : 'setTitle'
-                  }
-                  type="button"
-                  className={
-                    classes('button', { 'is-info': good === title })
-                  }
-                  onClick={() => handleTitleChange(good)}
-                >
-                  {good === title
-                    ? '-'
-                    : '+'
-                  }
-                </button>
-              </td>
+          {goods.map((good) => {
+            const isSelectedGood = good === title;
 
-              <td
-                data-cy="GoodTitle"
-                className="is-vcentered"
+            return (
+              <tr
+                data-cy="Good"
+                key={uuidv4()}
+                className={
+                  classes({ 'has-background-success-light': isSelectedGood })
+                }
               >
-                {good}
-              </td>
-            </tr>
-          ))}
+                <td>
+                  <button
+                    data-cy={isSelectedGood
+                      ? 'RemoveButton'
+                      : 'AddButton'
+                    }
+                    aria-label={isSelectedGood
+                      ? 'clearTitle'
+                      : 'setTitle'
+                    }
+                    type="button"
+                    className={
+                      classes('button', { 'is-info': isSelectedGood })
+                    }
+                    onClick={() => handleTitleChange(good)}
+                  >
+                    {isSelectedGood
+                      ? '-'
+                      : '+'
+                    }
+                  </button>
+                </td>
+
+                <td
+                  data-cy="GoodTitle"
+                  className="is-vcentered"
+                >
+                  {good}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </main>
