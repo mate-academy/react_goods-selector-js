706952a21245e1663047b4e54283c95c83ffe6d7
Ivanna Zaiets <iva.zaiets@gmail.com>
2023-10-10 15:55:24 +0300

    fixed some mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export const goods = [
   'Dumplings',
@@ -16,67 +16,71 @@ export const goods = [
 ];
 
 export const App = () => {
-  const [food, setFood] = useState('Jam');
+  const [selectedGood, setSelectedGood] = useState('Jam');
 
   return (
     <main className="section container">
-      {food.length !== 0 ? (
+      <h1 className="title is-flex is-align-items-center">
+        {selectedGood.length !== 0 ? (
+          <>
+            {`${selectedGood} is selected`}
 
-        <h1 className="title is-flex is-align-items-center">
-          {`${food} is selected`}
+            <button
+              onClick={() => setSelectedGood('')}
+              data-cy="ClearButton"
+              type="button"
+              className="delete ml-3"
+            />
+          </>
+        ) : (
+          `No goods selected`
+        )}
+      </h1>
+      <table className="table">
+        <tbody>
+          {goods.map(good => (
+            <React.Fragment key={good}>
+              {selectedGood === good ? (
+                <tr
+                  data-cy="Good"
+                  className="has-background-success-light"
+                >
+                  <td>
+                    <button
+                      onClick={() => setSelectedGood('')}
+                      data-cy="RemoveButton"
+                      type="button"
+                      className="button is-info"
+                    >
+                      -
+                    </button>
+                  </td>
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {good}
+                  </td>
+                </tr>
+              ) : (
+                <tr data-cy="Good">
+                  <td>
+                    <button
+                      onClick={() => setSelectedGood(good)}
+                      data-cy="AddButton"
+                      type="button"
+                      className="button"
+                    >
+                      +
+                    </button>
+                  </td>
 
-          <button
-            onClick={() => setFood('')}
-            data-cy="ClearButton"
-            type="button"
-            className="delete ml-3"
-          />
-        </h1>
-      ) : (
-        <h1 className="title is-flex is-align-items-center">
-          No goods selected
-        </h1>
-      )}
-      {goods.map(good => (
-        <table className="table">
-          <tbody>
-            {food === good ? (
-              <tr data-cy="Good" className="has-background-success-light">
-                <td>
-                  <button
-                    onClick={() => setFood('')}
-                    data-cy="RemoveButton"
-                    type="button"
-                    className="button is-info"
-                  >
-                    -
-                  </button>
-                </td>
-                <td data-cy="GoodTitle" className="is-vcentered">
-                  {good}
-                </td>
-              </tr>
-            ) : (
-              <tr data-cy="Good">
-                <td>
-                  <button
-                    onClick={() => setFood(good)}
-                    data-cy="AddButton"
-                    type="button"
-                    className="button"
-                  >
-                    +
-                  </button>
-                </td>
-
-                <td data-cy="GoodTitle" className="is-vcentered">
-                  {good}
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      ))}
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {good}
+                  </td>
+                </tr>
+              )}
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 };
