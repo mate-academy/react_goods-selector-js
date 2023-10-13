6dde7da074798eb10dc0f567c7e9916c6b05853a
Dmytro Panin <dmitriyp1598@gmail.com>
2023-10-13 16:36:53 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -20,58 +20,59 @@ export const App = () => {
 
   return (
     <main className="section container">
-      {food.length !== 0 ? (
-        /* eslint-disable */
-        <h1 className="title is-flex is-align-items-center">
-          {`${food} is selected`}
-          <button
-            data-cy="ClearButton"
-            type="button"
-            className="delete ml-3"
-            onClick={() => setFood('')}
-          />
-        </h1>
-      ) : (
-        <h1 className="title is-flex is-align-items-center">No goods selected</h1>
-      )}
+      <h1 className="title is-flex is-align-items-center">
+        {food
+          ? (
+            <>
+              {`${food} is selected`}
+              <button
+                data-cy="ClearButton"
+                type="button"
+                className="delete ml-3"
+                onClick={() => setFood('')}
+              />
+            </>
+          )
+          : (
+            'No goods selected'
+          )}
+      </h1>
 
       {goods.map(good => (
         <table className="table">
           <tbody>
-            {food === good ? (
-              <tr data-cy="Good" className="has-background-success-light">
-                <td>
-                  <button
-                    data-cy="RemoveButton"
-                    type="button"
-                    className="button is-info"
-                    onClick={() => setFood('')}
-                  >
-                    -
-                  </button>
-                </td>
+            <tr data-cy="Good" className={`${food === good ? 'has-background-success-light' : ''}`}>
+              {food === good
+                ? (
+                  <td>
+                    <button
+                      data-cy="RemoveButton"
+                      type="button"
+                      className="button is-info"
+                      onClick={() => setFood('')}
+                    >
+                      -
+                    </button>
+                  </td>
+                )
+                : (
+                  <td>
+                    <button
+                      data-cy="AddButton"
+                      type="button"
+                      className="button"
+                      onClick={() => setFood(good)}
+                    >
+                      +
+                    </button>
+                  </td>
+                )
+              }
 
-                <td data-cy="GoodTitle" className="is-vcentered">
-                  {good}
-                </td>
-              </tr>) : (
-              <tr data-cy="Good">
-                <td>
-                  <button
-                    data-cy="AddButton"
-                    type="button"
-                    className="button"
-                    onClick={() => setFood(good)}
-                  >
-                    +
-                  </button>
-                </td>
-
-                <td data-cy="GoodTitle" className="is-vcentered">
-                  {good}
-                </td>
-              </tr>
-            )}
+              <td data-cy="GoodTitle" className="is-vcentered">
+                {good}
+              </td>
+            </tr>
           </tbody>
         </table>
       ))}
