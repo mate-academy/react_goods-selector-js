1b47005a912a42bd273a1d6e11a68604b828141e
Zhekich123 <axelb380@gmail.com>
2023-10-17 22:35:00 +0300

    fix remove button

--- src/App.jsx
+++ src/App.jsx
@@ -68,6 +68,9 @@ export const App = () => {
                       data-cy="RemoveButton"
                       type="button"
                       className="button is-info"
+                      onClick={() => {
+                        setValue('');
+                      }}
                     >
                       -
                     </button>
