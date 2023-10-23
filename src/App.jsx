5186d6dc7c12ea10718ad4e2a1777bac9728763a
Kateryna Sydorenko <katerynnnna@gmail.com>
2023-10-23 20:40:49 +0300

    fix solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
 import { useState } from 'react';
+import classnames from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -20,24 +21,17 @@ export const App = () => {
 
   return (
     <main className="section container">
-      {selectedGood === ''
-        ? (
-          <h1 className="title is-flex is-align-items-center">
-            No goods selected
-          </h1>
-        )
-        : (
-          <h1 className="title is-flex is-align-items-center">
-            {`${selectedGood} is selected`}
-
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={() => setSelectedGood('')}
-            />
-          </h1>
+      <h1 className="title is-flex is-align-items-center">
+        {selectedGood ? `${selectedGood} is selected` : 'No goods selected'}
+        {selectedGood && (
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => setSelectedGood('')}
+          />
         )}
+      </h1>
 
       <table className="table">
         <tbody>
@@ -45,9 +39,9 @@ export const App = () => {
             <tr
               data-cy="Good"
               key={good}
-              className={selectedGood === good
-                ? 'has-background-success-light'
-                : 'is-vcentered'}
+              className={classnames({
+                'has-background-success-light': selectedGood === good,
+              })}
             >
               {selectedGood === good
                 ? (
