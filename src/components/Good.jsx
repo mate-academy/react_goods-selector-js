db69d2295fe95a92f71bef1c274647a8424329c2
Viktoriia Soloviova <ivicki.vs@gmail.com>
2023-11-06 21:39:20 +0200

    +cn

--- src/components/Good.jsx
+++ src/components/Good.jsx
@@ -1,39 +1,38 @@
-export const Good = ({ good, selectedGood, setSelectedGood }) => {
-  const itemClassName = selectedGood === good
-    ? 'has-background-success-light'
-    : '';
+import cn from 'classnames';
 
-  return (
-    <tr data-cy="Good" className={itemClassName}>
-      <td>
-        {selectedGood !== good ? (
-          <button
-            data-cy="AddButton"
-            type="button"
-            className="button"
-            onClick={() => {
-              setSelectedGood(good);
-            }}
-          >
-            +
-          </button>
-        ) : (
-          <button
-            data-cy="RemoveButton"
-            type="button"
-            className="button is-info"
-            onClick={() => {
-              setSelectedGood('');
-            }}
-          >
-            -
-          </button>
-        )}
-      </td>
+export const Good = ({ good, selectedGood, setSelectedGood }) => (
+  <tr
+    data-cy="Good"
+    className={cn({ 'has-background-success-light': selectedGood === good })}
+  >
+    <td>
+      {selectedGood !== good ? (
+        <button
+          data-cy="AddButton"
+          type="button"
+          className="button"
+          onClick={() => {
+            setSelectedGood(good);
+          }}
+        >
+          +
+        </button>
+      ) : (
+        <button
+          data-cy="RemoveButton"
+          type="button"
+          className="button is-info"
+          onClick={() => {
+            setSelectedGood('');
+          }}
+        >
+          -
+        </button>
+      )}
+    </td>
 
-      <td data-cy="GoodTitle" className="is-vcentered">
-        {good}
-      </td>
-    </tr>
-  );
-};
+    <td data-cy="GoodTitle" className="is-vcentered">
+      {good}
+    </td>
+  </tr>
+);
