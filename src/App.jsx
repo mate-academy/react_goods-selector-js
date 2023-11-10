9762e2618a9d91a06a3d53f1b55cf95a3c826500
Valeriia Fomenko <valeriia.fomenko@ukr.net>
2023-11-10 13:27:27 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
 import { useState } from 'react';
+import cn from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -20,7 +21,7 @@ export const App = () => {
 
   return (
     <main className="section container">
-      {selectedGood === '' ? (
+      {!selectedGood ? (
         <h1
           className="title is-flex is-align-items-center"
         >
@@ -44,9 +45,9 @@ export const App = () => {
             <tr
               data-cy="Good"
               key={good}
-              className={
-                selectedGood === good ? 'has-background-success-light' : ''
-              }
+              className={cn({
+                'has-background-success-light': selectedGood === good,
+              })}
             >
               <td>
                 {selectedGood === good ? (
