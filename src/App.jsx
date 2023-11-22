9abfab2b4fa4f1bdd52d0ee51046c693546d5ef2
Anton Kuchmasov <antek.ukraine@gmail.com>
2023-11-22 16:50:52 +0200

    fixed tiny bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+
+import cn from 'classnames';
 import { useState } from 'react';
 
 export const goods = [
@@ -21,23 +23,23 @@ export const App = () => {
   return (
     <main className="section container">
       {!selectedGood && (
-      <h1 className="title is-flex is-align-items-center">
-        No goods selected
-      </h1>
+        <h1 className="title is-flex is-align-items-center">
+          No goods selected
+        </h1>
       )}
 
       {selectedGood && (
-      <h1 className="title is-flex is-align-items-center">
-        {`${selectedGood} `}
-        is selected
+        <h1 className="title is-flex is-align-items-center">
+          {`${selectedGood} `}
+          is selected
 
-        <button
-          data-cy="ClearButton"
-          type="button"
-          className="delete ml-3"
-          onClick={() => (setSelectedGood(''))}
-        />
-      </h1>
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => setSelectedGood('')}
+          />
+        </h1>
       )}
 
       <table className="table">
@@ -46,9 +48,7 @@ export const App = () => {
             <tr
               data-cy="Good"
               className={
-                selectedGood === good
-                  ? 'has-background-success-light'
-                  : ''
+                cn({ 'has-background-success-light': selectedGood === good })
               }
             >
               <td>
