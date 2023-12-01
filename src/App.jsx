fb927ef4c43bdc7bd8c3e2b9f96afcbf713ca0b2
Vladimir Kolesnikov <9858713@gmail.com>
2023-12-01 10:01:05 +0200

    remove unuseful comment

--- src/App.jsx
+++ src/App.jsx
@@ -80,38 +80,6 @@ export const App = () => {
               </td>
             </tr>
           ))}
-
-          {/* <tr data-cy="Good" className="has-background-success-light">
-            <td>
-              <button
-                data-cy="RemoveButton"
-                type="button"
-                className="button is-info"
-              >
-                -
-              </button>
-            </td>
-
-            <td data-cy="GoodTitle" className="is-vcentered">
-              Jam
-            </td>
-          </tr>
-
-          <tr data-cy="Good">
-            <td>
-              <button
-                data-cy="AddButton"
-                type="button"
-                className="button"
-              >
-                +
-              </button>
-            </td>
-
-            <td data-cy="GoodTitle" className="is-vcentered">
-              Garlic
-            </td>
-          </tr> */}
         </tbody>
       </table>
     </main>
