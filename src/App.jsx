b92cd6a0cbf90d2b983928072d810eb371adb582
TVChulkova <6.02tatyana@gmail.com>
2023-12-17 20:40:56 +0200

    added function reset

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
   const titleDefaultText = 'No goods selected';
   const titleSelectedText = `${selectedGood} is selected`;
+  const reset = () => setSelectedGood('');
 
   return (
     <main className="section container">
@@ -30,9 +31,7 @@ export const App = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => {
-              setSelectedGood('');
-            }}
+            onClick={reset}
           />
         )}
       </h1>
@@ -60,9 +59,7 @@ export const App = () => {
                           data-cy="RemoveButton"
                           type="button"
                           className="button is-info"
-                          onClick={() => {
-                            setSelectedGood('');
-                          }}
+                          onClick={reset}
                         >
                           -
                         </button>
