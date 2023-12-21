fbc76d26a41aafd9b0e14015e711f7e2c9df6fa2
RVDotsenko <docenkoromaha@gmail.com>
2023-12-21 19:32:32 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goods = [
   'Dumplings',
@@ -14,70 +15,68 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [goodState, setGood] = useState('Jam');
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+  return (
+    <main className="section container">
+      <h1 className="title is-flex is-align-items-center">
+        <span>
+          {goodState === '' ? 'No goods selected' : `${goodState} is selected`}
+        </span>
 
+        {goodState === ''
+          ? ''
+          : (
+            <button
+              data-cy="ClearButton"
+              type="button"
+              className="delete ml-3"
+              onClick={() => setGood('')}
+            />
+          )
+        }
+      </h1>
+
+      <Table setGood={setGood} goodState={goodState} />
+    </main>
+  );
+};
+
+export const Table = ({ setGood, goodState }) => (
+  <table className="table">
+    <tbody>
+      {goods.map(good => (
+        <Good setGood={setGood} good={good} goodState={goodState} key={good} />
+      ))}
+    </tbody>
+  </table>
+);
+
+export const Good = ({ goodState, setGood, good }) => (
+  <tr
+    data-cy="Good"
+    className={`${goodState === good
+      ? 'has-background-success-light'
+      : ''}`}
+  >
+    <td>
       <button
-        data-cy="ClearButton"
+        data-cy={`${goodState === good ? 'RemoveButton' : 'AddButton'}`}
         type="button"
-        className="delete ml-3"
-      />
-    </h1>
+        className={`button ${goodState === good
+          ? 'is-info'
+          : ''}`}
+        onClick={() => (
+          goodState === good ? setGood('') : setGood(good)
+        )}
+      >
+        {goodState === good ? '-' : '+'}
+      </button>
+    </td>
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
-
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
+    <td data-cy="GoodTitle" className="is-vcentered">
+      {good}
+    </td>
+  </tr>
 );
