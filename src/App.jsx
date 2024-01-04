05217b771c0e525eba8a3947a2e32554196e9d37
ValeriaMyronets <valery.mir.11@gmail.com>
2024-01-04 23:21:02 +0200

    add key

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ export const App = () => {
           {goods.map(good => (
             <tr
               data-cy="Good"
+              key={good}
               className={
                 selectedGood === good ? 'has-background-success-light' : ''
               }
