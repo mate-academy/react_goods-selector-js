d225fdf21b2f4bf2edbd4c986cf4150ba84a0826
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2024-01-21 15:08:30 +0200

    everything works except btns, multiple choice is available

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import 'bulma/css/bulma.css';
+import cn from 'classnames';
 import './App.scss';
 
 export const goods = [
@@ -14,70 +16,169 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const ItemGood = (
+  {
+    good, selectedGood, setselectedGood,
+  },
+) => {
+  const [btnClickVerification, setBtnClVer] = useState(false);
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+  return (
+    <tr data-cy="Good" className={`${btnClickVerification && 'has-background-success-light'}`}>
+      <td>
+        <button
+          data-cy={`${btnClickVerification ? 'RemoveButton' : 'AddButton'}`}
+          type="button"
+          className={cn('button', {
+            'is-info': btnClickVerification,
+          })}
+          onClick={
+            ({ type }) => {
+              if (type === 'click') {
+                setBtnClVer(true);
+                setselectedGood(good);
+              }
+            }
+          }
+        >
+          {`${btnClickVerification ? '-' : '+'}`}
+        </button>
+      </td>
 
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+      <td data-cy="GoodTitle" className="is-vcentered">
+        {good}
+      </td>
+    </tr>
+  );
+};
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+export const App = () => {
+  const [selectedGood, setselectedGood] = useState('Jam');
+  const [goodsMod] = useState(goods);
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+  const setTextTitleAppear = () => {
+    const showNoSelectionStr = document.querySelectorAll('.title')[0];
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
+    if (selectedGood === '') {
+      showNoSelectionStr.classList.remove('is-hidden');
+    } else if (selectedGood !== '') {
+      showNoSelectionStr.classList.add('is-hidden');
+    }
+  };
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
+  return (
+    <main className="section container">
+      <h1 className={
+        `title is-flex is-align-items-center ${selectedGood ? 'is-hidden' : ''}`
+        }
+      >
+        No goods selected
+      </h1>
 
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+      <h1 className={
+        `title is-flex is-align-items-center ${!selectedGood ? 'is-hidden' : ''}`
+        }
+      >
+        {`${selectedGood} is selected`}
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+        <button
+          data-cy="ClearButton"
+          type="button"
+          className="delete ml-3"
+          onClick={
+            () => {
+              setselectedGood('');
+              setTextTitleAppear();
+            }
+          }
+        />
+      </h1>
+
+      <table className="table">
+        <tbody>
+          {goodsMod.map(item => (
+            <ItemGood
+              key={item}
+              good={item}
+              selectedGood={selectedGood}
+              setselectedGood={setselectedGood}
+              setTextTitleAppear={setTextTitleAppear}
+            />
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+};
+
+export const App1 = () => {
+  const [good] = useState('Jam is selected');
+
+  return (
+    <main className="section container">
+      <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+
+      <h1 className="title is-flex is-align-items-center">
+        {good}
+
+        <button
+          data-cy="ClearButton"
+          type="button"
+          className="delete ml-3"
+        />
+      </h1>
+
+      <table className="table">
+        <tbody>
+          <tr data-cy="Good">
+            <td>
+              <button
+                data-cy="AddButton"
+                type="button"
+                className="button"
+              >
+                +
+              </button>
+            </td>
+
+            <td data-cy="GoodTitle" className="is-vcentered">
+              Dumplings
+            </td>
+          </tr>
+
+          <tr data-cy="Good" className="has-background-success-light">
+            <td>
+              <button
+                data-cy="RemoveButton"
+                type="button"
+                className="button is-info"
+              >
+                -
+              </button>
+            </td>
+
+            <td data-cy="GoodTitle" className="is-vcentered">
+              Jam
+            </td>
+          </tr>
+
+          <tr data-cy="Good">
+            <td>
+              <button
+                data-cy="AddButton"
+                type="button"
+                className="button"
+              >
+                +
+              </button>
+            </td>
+
+            <td data-cy="GoodTitle" className="is-vcentered">
+              Garlic
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
+// import { App1 } from './App';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
 );
+
+// ReactDOM.render(
+//   <App1 />,
+//   document.getElementById('root'),
+// );
