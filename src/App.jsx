6352ade16bd788842367345b4784f92e6dbf8219
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2024-01-28 14:16:32 +0200

    add something important

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import 'bulma/css/bulma.css';
 import cn from 'classnames';
+import { ItemGood } from './components/ItemGood';
 import './App.scss';
 
 export const goods = [
@@ -16,73 +17,6 @@ export const goods = [
   'Garlic',
 ];
 
-export const ItemGood = ({
-  good,
-  goodsMod,
-  goodsCh,
-  setGoodsCh,
-}) => {
-  const { id, name, bool } = good;
-
-  function setNewValues() {
-    const itemHasTrueId
-    = goodsCh.find(i => i.bool === true)
-      ? goodsCh.find(i => i.bool === true).id
-      : 0;
-
-    if (itemHasTrueId !== id) {
-      const a = goodsMod.map(e => ({
-        ...e, bool: false,
-      }));
-      const chosen = a.find(e => e.id === id);
-
-      chosen.bool = true;
-
-      setGoodsCh(a);
-    } else if (itemHasTrueId === id) {
-      const a = goodsMod.map(e => ({
-        ...e, bool: false,
-      }));
-
-      setGoodsCh(a);
-    }
-  }
-
-  const btnClickAction = function clickAct({ type }) {
-    if (type === 'click') {
-      setNewValues();
-    }
-  };
-
-  return (
-    <tr
-      data-cy="Good"
-      className={cn({
-        'has-background-success-light': bool,
-      })}
-    >
-      <td>
-        <button
-          data-cy={`${bool ? 'RemoveButton' : 'AddButton'}`}
-          type="button"
-          className={cn('button', {
-            'is-info': bool,
-          })}
-          onClick={
-            btnClickAction
-          }
-        >
-          {`${bool ? '-' : '+'}`}
-        </button>
-      </td>
-
-      <td data-cy="GoodTitle" className="is-vcentered">
-        {name}
-      </td>
-    </tr>
-  );
-};
-
 export const App = () => {
   const goodsMod = goods.map(itemM => ({
     id: goods.indexOf(itemM) + 1, name: itemM, bool: false,
@@ -95,8 +29,8 @@ export const App = () => {
   const [goodsCh, setGoodsCh] = useState(goodsMod);
 
   const selectedName
-  = goodsCh.find(i => i.bool === true)
-    ? goodsCh.find(i => i.bool === true).name : '';
+  = goodsCh.find(i => i.bool)
+    ? goodsCh.find(i => i.bool).name : '';
 
   const setNoGoods = function setNo() {
     const a = goodsMod.map(e => ({
@@ -125,9 +59,7 @@ export const App = () => {
           data-cy="ClearButton"
           type="button"
           className="delete ml-3"
-          onClick={
-            setNoGoods
-          }
+          onClick={setNoGoods}
         />
       </h1>
 
