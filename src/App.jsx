8d0127ae5b2c365eb75599225adefeac1c5497ac
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2024-02-09 01:41:09 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goods = [
   'Dumplings',
@@ -14,70 +15,90 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [value, setValue] = useState('Jam');
+  const currentSelected = value;
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
+  return (
+    <main className="section container">
 
-      <button
-        data-cy="ClearButton"
-        type="button"
-        className="delete ml-3"
-      />
-    </h1>
+      {value !== 'No goods selected' ? (
+        <h1 className="title is-flex is-align-items-center">
+          {value}
+          {` is selected`}
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => {
+              setValue('No goods selected');
+            }}
+          />
+        </h1>
+      )
+        : (
+          <h1 className="title is-flex is-align-items-center">
+            {value}
+          </h1>
+        )}
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
+      <table className="table">
+        <tbody>
+          {goods.map(good => (
+            good === currentSelected ? (
+              <tr
+                data-cy="Good"
+                className="has-background-success-light"
+                key={good}
+              >
+                <td>
+                  <button
+                    type="button"
+                    className="button is-info"
+                    data-cy="RemoveButton"
+                    onClick={() => {
+                      setValue('No goods selected');
+                    }}
+                  >
+                    -
+                  </button>
+                </td>
+                <td
+                  data-cy="GoodTitle"
+                  className="is-vcentered"
+                >
+                  {good}
+                </td>
+              </tr>
+            ) : (
+              <tr
+                data-cy="Good"
+                key={good}
+              >
+                <td>
+                  <button
+                    type="button"
+                    className="button"
+                    data-cy="AddButton"
+                    onClick={() => {
+                      setValue(good);
+                    }}
+                  >
+                    +
+                  </button>
+                </td>
+                <td
+                  data-cy="GoodTitle"
+                  className="is-vcentered"
+                >
+                  {good}
+                </td>
+              </tr>
+            )
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
-
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+};
