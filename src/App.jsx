09b009e7b5f7be336a25479fc500bb8d1339d231
Maksym Prokopovych <prokopovych2006@gmail.com>
2024-02-27 17:32:18 +0200

    create-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
+import cn from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -14,30 +16,69 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [message, setMessage] = useState('No goods selected');
+  const [selectedGood, setSelectedGood] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(null);
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
-      <button data-cy="ClearButton" type="button" className="delete ml-3" />
-    </h1>
+  return (
+    <main className="section container">
+      {/* <h1 className="title is-flex is-align-items-center">No goods selected</h1> */}
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button data-cy="AddButton" type="button" className="button">
-              +
-            </button>
-          </td>
+      <h1 className="title is-flex is-align-items-center">
+        {message}
+        <button
+          data-cy="ClearButton"
+          type="button"
+          className="delete ml-3"
+          onClick={() => {
+            setMessage(`No goods selected`);
+            setSelectedGood(null);
+            setSelectedIndex(null);
+          }}
+        />
+      </h1>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+      <table className="table">
+        <tbody>
+          {goods.map((good, index) => {
+            return (
+              <>
+                <tr
+                  key={good}
+                  data-cy="Good"
+                  className={
+                    selectedIndex === index
+                      ? 'has-background-success-light'
+                      : ''
+                  }
+                >
+                  <td>
+                    <button
+                      data-cy="AddButton"
+                      type="button"
+                      className={cn('button', {
+                        'is-info': selectedIndex === index,
+                      })}
+                      onClick={() => {
+                        setMessage(`${good} is selected`);
+                        setSelectedGood(selectedGood === good ? null : good);
+                        setSelectedIndex(selectedGood === good ? null : index);
+                      }}
+                    >
+                      {selectedGood === good ? '-' : '+'}
+                    </button>
+                  </td>
 
-        <tr data-cy="Good" className="has-background-success-light">
+                  <td data-cy="GoodTitle" className="is-vcentered">
+                    {good}
+                  </td>
+                </tr>
+              </>
+            );
+          })}
+
+          {/* <tr data-cy="Good" className="has-background-success-light">
           <td>
             <button
               data-cy="RemoveButton"
@@ -63,8 +104,9 @@ export const App = () => (
           <td data-cy="GoodTitle" className="is-vcentered">
             Garlic
           </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+        </tr> */}
+        </tbody>
+      </table>
+    </main>
+  );
+};
