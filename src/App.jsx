53d4a37751eb2af1baf86486912fed7963d684ca
Dmitrel <153143020+Dmitrel@users.noreply.github.com>
2024-03-07 16:39:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export const goods = [
 ];
 
 export const App = () => {
-  const [selectedGood, setSelectedGood] = useState('Jam');
+  const [selectedGood, setSelectedGood] = useState(goods[8]);
 
   return (
     <main className="section container">
