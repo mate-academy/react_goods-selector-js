18c6460ac013ae138bbcef5f6d286e663cf6bc6e
Anastasiia <kolomiecnastia8932@gmail.com>
2024-03-11 16:19:47 +0200

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,7 @@ export const App = () => {
             return (
               <tr
                 data-cy="Good"
+                key={good.id}
                 className={classNames({
                   'has-background-success-light': isSelected,
                 })}
@@ -70,50 +71,4 @@ export const App = () => {
       </table>
     </main>
   );
-
-  // <main className="section container">
-  //   <table className="table">
-  //     {/* <tbody>
-  //       <tr data-cy="Good">
-  //         <td>
-  //           <button data-cy="AddButton" type="button" className="button">
-  //             +
-  //           </button>
-  //         </td>
-
-  // <td data-cy="GoodTitle" className="is-vcentered">
-  //   Dumplings
-  // </td>
-  //       </tr>
-
-  //       <tr data-cy="Good" className="has-background-success-light">
-  //         <td>
-  //           <button
-  //             data-cy="RemoveButton"
-  //             type="button"
-  //             className="button is-info"
-  //           >
-  //             -
-  //           </button>
-  //         </td>
-
-  //         <td data-cy="GoodTitle" className="is-vcentered">
-  //           Jam
-  //         </td>
-  //       </tr>
-
-  //       <tr data-cy="Good">
-  //         <td>
-  //           <button data-cy="AddButton" type="button" className="button">
-  //             +
-  //           </button>
-  //         </td>
-
-  //         <td data-cy="GoodTitle" className="is-vcentered">
-  //           Garlic
-  //         </td>
-  //       </tr>
-  //     </tbody> */}
-  //   </table>
-  // </main>
 };
