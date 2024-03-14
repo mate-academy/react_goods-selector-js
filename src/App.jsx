605e568e077d2714d4ddb86090c1c10444ff7084
deezhizyu <kalenskuja@gmail.com>
2024-03-14 15:50:13 +0100

    Add key prop

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ export const App = () => {
           {goods.map(good => (
             <tr
               data-cy="Good"
+              key={good}
               className={
                 selectedGood === good && 'has-background-success-light'
               }
