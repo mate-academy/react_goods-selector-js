3dbd164a9fe75420e75e973baa3956388b671af3
Ivan <110199ws@gmail.com>
2024-04-10 14:40:47 +0300

    selection works good

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goods = [
   'Dumplings',
@@ -14,57 +15,76 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [selected, setSelected] = useState(false);
+  const [selectProduct, setSelectProduct] = useState(null);
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
-      <button data-cy="ClearButton" type="button" className="delete ml-3" />
-    </h1>
+  function handleSelection(product) {
+    let select = true;
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button data-cy="AddButton" type="button" className="button">
-              +
-            </button>
-          </td>
+    if (!product) {
+      select = false;
+    }
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
+    setSelectProduct(product);
+    setSelected(select);
+  }
 
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
+  return (
+    <main className="section container">
+      {selected ? (
+        <h1 className="title is-flex is-align-items-center">
+          {selectProduct} is selected
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => setSelected(false)}
+          />
+        </h1>
+      ) : (
+        <h1 className="title is-flex is-align-items-center">
+          No goods selected
+        </h1>
+      )}
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
+      <table className="table">
+        <tbody>
+          <tr data-cy="Good">
+            <td>
+              <button
+                data-cy="AddButton"
+                type="button"
+                className="button"
+                onClick={() => handleSelection(goods[0])}
+              >
+                +
+              </button>
+            </td>
 
-        <tr data-cy="Good">
-          <td>
-            <button data-cy="AddButton" type="button" className="button">
-              +
-            </button>
-          </td>
+            <td data-cy="GoodTitle" className="is-vcentered">
+              {goods[0]}
+            </td>
+          </tr>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+          <tr data-cy="Good" className="has-background-success-light">
+            <td>
+              <button
+                data-cy="RemoveButton"
+                type="button"
+                className="button is-info"
+                onClick={() => handleSelection(null)}
+              >
+                -
+              </button>
+            </td>
+
+            <td data-cy="GoodTitle" className="is-vcentered">
+              {goods[0]}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
+};
