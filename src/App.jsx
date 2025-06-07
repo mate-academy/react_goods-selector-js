f33442cd54d800f23a2f31553f344aec74265a8c
Michelle Tani <michelletani@outlook.com>
2025-06-07 17:57:59 -0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
-import "bulma/css/bulma.css";
-import "./App.scss";
+import React, { useState } from 'react';
+import 'bulma/css/bulma.css';
+import './App.scss';
 
 export const goods = [
-  "Dumplings",
-  "Carrot",
-  "Eggs",
-  "Ice cream",
-  "Apple",
-  "Bread",
-  "Fish",
-  "Honey",
-  "Jam",
-  "Garlic",
+  'Dumplings',
+  'Carrot',
+  'Eggs',
+  'Ice cream',
+  'Apple',
+  'Bread',
+  'Fish',
+  'Honey',
+  'Jam',
+  'Garlic',
 ];
 
 export const App = () => {
-  const [selectedGood, setSelectedGood] = useState("Jam");
+  const [selectedGood, setSelectedGood] = useState('Jam');
 
   return (
     <main className="section container">
       <h1 className="title is-flex is-align-items-center">
-        {selectedGood
-          ? `${selectedGood} is selected`
-          : "No goods selected"}
+        {selectedGood ? `${selectedGood} is selected` : 'No goods selected'}
         {selectedGood && (
           <button
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setSelectedGood("")}
+            onClick={() => setSelectedGood('')}
           />
         )}
       </h1>
 
       <table className="table">
         <tbody>
-          {goods.map((good) => (
+          {goods.map(good => (
             <tr
               key={good}
               data-cy="Good"
-              className={good === selectedGood ? "has-background-success-light" : ""}
+              className={
+                good === selectedGood ? 'has-background-success-light' : ''
+              }
             >
               <td>
                 {selectedGood !== good && (
@@ -58,7 +58,7 @@ export const App = () => {
                     data-cy="RemoveButton"
                     type="button"
                     className="button is-info"
-                    onClick={() => setSelectedGood("")}
+                    onClick={() => setSelectedGood('')}
                   >
                     -
                   </button>
