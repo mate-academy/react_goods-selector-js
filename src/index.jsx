3d8582699b651ac5e7f1aa1f63363808dca1ed2b
Emaan Hookey <em.grey28@gmail.com>
2025-08-03 19:44:25 -0230

    followed instructions

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
+import './App.scss';
 
-import { App } from './App';
-
-createRoot(document.getElementById('root')).render(<App />);
+const container = document.getElementById('root');
+createRoot(container).render(<App />);
