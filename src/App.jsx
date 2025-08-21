6a57e8f5c3b36d23b35f873d238dae8ee5f737db
danie010830 <danielgukillwar008@gmail.com>
2025-08-21 14:30:26 -0300

    corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -18,23 +18,33 @@ export const goods = [
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
 
+  const handleSelectGood = name => {
+    return () => {
+      setSelectedGood(name);
+    };
+  };
+
+  const handleClearSelection = () => {
+    setSelectedGood('');
+  };
+
   return (
     <main className="section container">
-      <h1 className="title is-flex is-align-items-center">
-        {selectedGood ? (
-          <>
-            {selectedGood} is selected
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={() => setSelectedGood('')}
-            />
-          </>
-        ) : (
-          'No goods selected'
-        )}
-      </h1>
+      {selectedGood === '' ? (
+        <h1 className="title is-flex is-align-items-center">
+          No goods selected
+        </h1>
+      ) : (
+        <h1 className="title is-flex is-align-items-center">
+          {selectedGood} is selected
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={handleClearSelection}
+          />
+        </h1>
+      )}
 
       <table className="table">
         <tbody>
@@ -52,7 +62,7 @@ export const App = () => {
                     data-cy="RemoveButton"
                     type="button"
                     className="button is-info"
-                    onClick={() => setSelectedGood('')}
+                    onClick={handleClearSelection}
                   >
                     -
                   </button>
@@ -61,7 +71,7 @@ export const App = () => {
                     data-cy="AddButton"
                     type="button"
                     className="button"
-                    onClick={() => setSelectedGood(good)}
+                    onClick={handleSelectGood(good)}
                   >
                     +
                   </button>
