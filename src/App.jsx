e5a30c776447e5f4c3cf350a67e2da1355d7cc04
Danylo <lliivadin@gmail.com>
2025-10-02 15:14:06 +0300

    fix: hide AddButton when a good is selected

--- src/App.jsx
+++ src/App.jsx
@@ -17,89 +17,63 @@ export const goods = [
 
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
-
-  function handleClearGood() {
-    setSelectedGood('');
-  }
+  const clear = () => setSelectedGood('');
 
   return (
     <main className="section container">
-      {selectedGood !== '' ? (
+      {!selectedGood ? (
         <h1 className="title is-flex is-align-items-center">
-          {`${selectedGood} is selected`}
+          No goods selected
+        </h1>
+      ) : (
+        <h1 className="title is-flex is-align-items-center">
+          {selectedGood} is selected
           <button
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={handleClearGood}
+            onClick={clear}
           />
-        </h1>
-      ) : (
-        <h1 className="title is-flex is-align-items-center">
-          No goods selected
         </h1>
       )}
 
       <table className="table">
         <tbody>
-          {goods.map(good => {
-            const allowAdd = selectedGood === '';
-
-            if (selectedGood === good) {
-              return (
-                <tr
-                  data-cy="Good"
-                  className="has-background-success-light"
-                  key={good}
-                >
-                  <td>
-                    <button
-                      data-cy="RemoveButton"
-                      type="button"
-                      className="button is-info"
-                      onClick={handleClearGood}
-                    >
-                      -
-                    </button>
-                  </td>
-
-                  <td data-cy="GoodTitle" className="is-vcentered">
-                    {good}
-                  </td>
-                </tr>
-              );
-            }
-
-            if (allowAdd) {
-              return (
-                <tr data-cy="Good" key={good}>
-                  <td>
-                    <button
-                      data-cy="AddButton"
-                      type="button"
-                      className="button"
-                      onClick={() => setSelectedGood(good)}
-                    >
-                      +
-                    </button>
-                  </td>
-
-                  <td data-cy="GoodTitle" className="is-vcentered">
-                    {good}
-                  </td>
-                </tr>
-              );
-            }
-
-            return (
-              <tr data-cy="Good" key={good}>
-                <td />
-                <td data-cy="GoodTitle" className="is-vcentered">
-                  {good}
-                </td>
-              </tr>
-            );
-          })}
+          {goods.map(good => (
+            <tr
+              data-cy="Good"
+              key={good}
+              className={
+                selectedGood === good ? 'has-background-success-light' : ''
+              }
+            >
+              <td>
+                {selectedGood === good && (
+                  <button
+                    data-cy="RemoveButton"
+                    type="button"
+                    className="button is-info"
+                    onClick={clear}
+                  >
+                    -
+                  </button>
+                )}
+                {!selectedGood && (
+                  <button
+                    data-cy="AddButton"
+                    type="button"
+                    className="button"
+                    onClick={() => setSelectedGood(good)}
+                  >
+                    +
+                  </button>
+                )}
+              </td>
+              <td data-cy="GoodTitle" className="is-vcentered">
+                {good}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </main>
