a11a471c745bfab14458f5e27096f2e0b6374551
MiltonMario <miltonneto0010@gmail.com>
2025-11-10 21:26:34 -0300

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,57 +15,66 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [selectedGood, setSelectedGood] = useState('Jam');
 
-    <h1 className="title is-flex is-align-items-center">
-      Jam is selected
-      <button data-cy="ClearButton" type="button" className="delete ml-3" />
-    </h1>
+  function handleClick(x) {
+    if (selectedGood === x) {
+      setSelectedGood('');
+    } else {
+      setSelectedGood(x);
+    }
+  }
 
-    <table className="table">
-      <tbody>
-        <tr data-cy="Good">
-          <td>
-            <button data-cy="AddButton" type="button" className="button">
-              +
-            </button>
-          </td>
+  return (
+    <main className="section container">
+      <h1 className="title is-flex is-align-items-center">
+        {selectedGood === ''
+          ? 'No goods selected'
+          : `${selectedGood} is selected`}
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Dumplings
-          </td>
-        </tr>
-
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
+        {selectedGood !== '' ? (
+          <button
+            onClick={() => {
+              setSelectedGood('');
+            }}
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            aria-label="Clear selected goods"
+          />
+        ) : null}
+      </h1>
+      <table className="table">
+        <tbody>
+          {goods.map(x => (
+            <tr
+              data-cy="Good"
+              key={x}
+              className={
+                selectedGood === x ? 'has-background-success-light' : ''
+              }
             >
-              -
-            </button>
-          </td>
+              <td>
+                <button
+                  data-cy={selectedGood === x ? 'RemoveButton' : 'AddButton'}
+                  type="button"
+                  className={selectedGood === x ? 'button is-info' : 'button'}
+                  onClick={() => {
+                    handleClick(x);
+                  }}
+                >
+                  {selectedGood === x ? '-' : '+'}
+                </button>
+              </td>
 
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Jam
-          </td>
-        </tr>
-
-        <tr data-cy="Good">
-          <td>
-            <button data-cy="AddButton" type="button" className="button">
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            Garlic
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-);
+              <td data-cy="GoodTitle" className="is-vcentered">
+                {x}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+};
