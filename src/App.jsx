9404dc93058236dd37c7b1a7f3ee4a8190146f3c
Evgenu <bogdanovevgenij87@gmail.com>
2026-01-21 18:03:47 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ export const App = () => {
         <tbody>
           {goods.map(good => (
             <tr
+              key={good}
               data-cy="Good"
               className={
                 good === selectedGood && 'has-background-success-light'
