093e7751331fe874f094acd6e2bf1441323a40e9
anastasiiadns <nastyadenisenko17@gmail.com>
2026-01-27 16:21:29 +0200

    Fixed "setValue" for RemoveButton and ClearButton from "undefined" to ''

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ export const App = () => {
                 data-cy="ClearButton"
                 type="button"
                 className="delete ml-3"
-                onClick={() => setValue(undefined)}
+                onClick={() => setValue('')}
               />
             </>
           )}
@@ -55,7 +55,7 @@ export const App = () => {
                     type="button"
                     className="button is-info"
                     onClick={() => {
-                      setValue(undefined);
+                      setValue('');
                     }}
                   >
                     -
