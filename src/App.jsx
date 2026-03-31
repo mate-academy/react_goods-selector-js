52c969ae24995334532b9a11c219b7bf489128b8
Diana Yevstignyeyeva <diana.yevstignyeyeva@gmail.com>
2026-03-31 15:51:04 +0300

    fix: clear the selection by setting an empty string to 'selectedGood'

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ export const App = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setSelectedGood(null)}
+            onClick={() => setSelectedGood('')}
           />
         </h1>
       )}
@@ -55,7 +55,7 @@ export const App = () => {
                     data-cy="RemoveButton"
                     type="button"
                     className="button is-info"
-                    onClick={() => setSelectedGood(null)}
+                    onClick={() => setSelectedGood('')}
                   >
                     -
                   </button>
