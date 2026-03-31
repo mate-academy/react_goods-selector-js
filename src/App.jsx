efd1e4f3c6267cbf41d51a473de9fa5a8294574f
Diana Yevstignyeyeva <diana.yevstignyeyeva@gmail.com>
2026-03-31 15:52:09 +0300

    fix: add the missing key in the React list

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,7 @@ export const App = () => {
         <tbody>
           {goods.map(good => (
             <tr
+              key={good}
               data-cy="Good"
               className={cn({
                 'has-background-success-light': selectedGood === good,
