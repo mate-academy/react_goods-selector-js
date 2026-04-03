910e2db91a3941014fbe036a9502bc81522e094e
Natalia Humenyk <gumenuknatalia53@gmail.com>
2026-04-03 21:19:51 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goods = [
   'Dumplings',
@@ -14,8 +15,68 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
+export const App = () => {
+  const [selectedGoods, setSelectedGoods] = useState('Jam');
+
+  return (
+    <main className="section container">
+      {}
+      <h1 className="title is-flex is-align-items-center">
+        {selectedGoods ? `${selectedGoods} is selected` : 'No goods selected'}
+        {selectedGoods && (
+          <button
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+            onClick={() => setSelectedGoods('')}
+          />
+        )}
+      </h1>
+
+      <table className="table">
+        <tbody>
+          {goods.map(good => (
+            <tr
+              key={good}
+              data-cy="Good"
+              className={
+                good === selectedGoods ? 'has-background-success-light' : ''
+              }
+            >
+              <td>
+                {' '}
+                {good === selectedGoods ? (
+                  <button
+                    data-cy="RemoveButton"
+                    type="button"
+                    className="button is-info"
+                    onClick={() => setSelectedGoods('')}
+                  >
+                    -
+                  </button>
+                ) : (
+                  <button
+                    data-cy="AddButton"
+                    type="button"
+                    className="button"
+                    onClick={() => setSelectedGoods(good)}
+                  >
+                    +
+                  </button>
+                )}
+              </td>
+
+              <td data-cy="GoodTitle" className="is-vcentered">
+                {good}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+};
+/* <main className="section container">
     <h1 className="title is-flex is-align-items-center">No goods selected</h1>
 
     <h1 className="title is-flex is-align-items-center">
@@ -67,4 +128,4 @@ export const App = () => (
       </tbody>
     </table>
   </main>
-);
+); */
