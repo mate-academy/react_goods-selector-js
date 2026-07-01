d6325ecd668e67160a784580380fddebb57518d9
tetbuk <tetiana.bukoros@dreamhost.com>
2026-07-01 16:06:46 +0300

    add key

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,7 @@ export const App = () => {
           {goods.map(good => (
             <tr
               data-cy="Good"
+              key={good}
               className={`${selectedGood === good ? 'has-background-success-light' : ''}`}
             >
               <td>
