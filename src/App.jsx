9339b640e70c416e04b2905ce6433ce477f965d8
Anton Ustavshchykov <unton84@gmail.com>
2026-08-06 18:07:12 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -16,49 +16,54 @@ export const goods = [
 ];
 
 export const App = () => {
-  const [selectedState, setState] = useState('Jam');
-  const clearButton = document.querySelector('[data-cy="ClearButton"]');
+  const [selectedGood, setState] = useState('Jam');
+  const [activeIndex, setActiveIndex] = useState(8);
+  const [isActive, setButtonState] = useState(true);
+
+  const handleToggle = index => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
 
   return (
     <main className="section container">
-      <h1 className="title is-flex is-align-items-center">No goods selected</h1>
-
       <h1 className="title is-flex is-align-items-center">
-        {selectedState} is selected
-        <button
-          onClick={() => {
-            setState('');
-          }}
-          data-cy="ClearButton"
-          type="button"
-          className="delete ml-3"
-          // style={{ display: 'none' }}
-        />
+        {selectedGood !== ''
+          ? `${selectedGood} is selected`
+          : 'No goods selected'}
+        {selectedGood && (
+          <button
+            onClick={() => {
+              setState('');
+            }}
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+          />
+        )}
       </h1>
 
       <table className="table">
         <tbody>
-          {goods.map(x => (
-            <tr data-cy={x}>
+          {goods.map((x, index) => (
+            <tr
+              className={
+                activeIndex === index ? 'has-background-success-light' : null
+              }
+              data-cy={x}
+              key={x}
+            >
               <td>
                 <button
                   onClick={() => {
                     setState(x);
-
-                    const tr = document.querySelector(`[data-cy=${x}]`);
-
-                    tr.className = 'has-background-success-light';
-                    clearButton.style.display = 'inline-block';
-                    clearButton.onClick = () => {
-                      setState('');
-                      tr.classList.remove('has-background-success-light');
-                    };
+                    handleToggle(index);
+                    setButtonState(!isActive);
                   }}
                   data-cy="AddButton"
                   type="button"
                   className="button"
                 >
-                  +
+                  {activeIndex === index ? '-' : '+'}
                 </button>
               </td>
 
@@ -67,53 +72,6 @@ export const App = () => {
               </td>
             </tr>
           ))}
-
-          {/* <tr data-cy="Good">
-            <td>
-              <button
-                onClick={() => {
-                  setState('Dumplings');
-                }}
-                data-cy="AddButton"
-                type="button"
-                className="button"
-              >
-                +
-              </button>
-            </td>
-
-            <td data-cy="GoodTitle" className="is-vcentered">
-              Dumplings
-            </td>
-          </tr>
-
-          <tr data-cy="Good" className="has-background-success-light">
-            <td>
-              <button
-                data-cy="RemoveButton"
-                type="button"
-                className="button is-info"
-              >
-                -
-              </button>
-            </td>
-
-            <td data-cy="GoodTitle" className="is-vcentered">
-              Jam
-            </td>
-          </tr>
-
-          <tr data-cy="Good">
-            <td>
-              <button data-cy="AddButton" type="button" className="button">
-                +
-              </button>
-            </td>
-
-            <td data-cy="GoodTitle" className="is-vcentered">
-              Garlic
-            </td>
-          </tr> */}
         </tbody>
       </table>
     </main>
